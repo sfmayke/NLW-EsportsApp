50d3d73a652376a56315b0c4773fa3272fe5656e
Mayke F. Santos <sfmayke@gmail.com>
2022-09-27 21:31:57 -0300

    fonts added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
         "nwl-gradient": 'linear-gradient(90deg, #E872FC, #43E7AD, #E1D55D)',
         "card-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)"
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'] 
+      }
     },
   },
   plugins: [],
